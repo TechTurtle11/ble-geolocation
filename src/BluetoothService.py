2e65020707f6edf7ea8fee39514a8cd679f8c11d
Thomas Barker <thomasjakebarker@outlook.com>
2021-11-17 16:47:08 +0000

    parsing improved

--- src/BluetoothService.py
+++ src/BluetoothService.py
@@ -1,4 +1,5 @@
 import subprocess
+import parse
 
 
 BEACON_PREFIX = "geolocation_beacon"
@@ -14,23 +15,27 @@ class BluetoothService():
         if process.returncode == 0:
             device_strings = process.stdout.replace(
                 "\n", "").split("hci0 dev_found: ")[1:]
+            #print(process.stdout)
+
+            c = parse.compile("\nhci0 dev_found: {address} type {type:S} rssi {rssi:d} flags {flags:x} \nname {name:S}")
+            for each in c.findall(process.stdout):
+                print(each)
             devices = [self.parse_device_string(
                 device_string) for device_string in device_strings]
-            print(devices)
+            #'print(devices)
         else:
             print(f"bluetooth was unable to scan for devices")
 
     def parse_device_string(self, device_string):
         device = {}
         words = device_string.split()
-        device["address"] = words[0]
-        device["type"] = words[2]
-        device["rssi"] = int(words[5])
-        try:
-            index = words.index("name")
-            device["name"] = " ".join(words[index+1:])
-        except ValueError:
-            device["name"] = "none"
+        #device["address"] = words[0]
+        device["address"] = ":".join(parse.search("{}:{}:{}:{}:{}:{} ",device_string).fixed)
+        device["type"] = parse.search("type {:S} ", device_string).fixed[0]
+        device["rssi"] = parse.search("rssi {:d} ", device_string).fixed[0]
+        name_parse = parse.search("name {:S}",device_string)
+        device["name"] = name_parse.fixed[0] if isinstance(name_parse, parse.Result) else None
+
         return device
 
 
