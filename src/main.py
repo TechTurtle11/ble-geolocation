0939f5ba5b0053917d857f815480eac059d98d4c
Thomas Barker <thomasbarker2011@hotmail.co.uk>
2021-12-15 14:44:44 +0000

    basic implmentation of paper

--- src/main.py
+++ src/main.py
@@ -1,7 +1,4 @@
-from collections import Set
 import logging
-import numpy as np
-from bluetooth.ble import BeaconService
 
 
 class Beacon:
@@ -57,7 +54,8 @@ class Map:
 
     def add_beacon(self, uid, location):
         if uid in self._beacons.keys():
-            logging.warning("a beacon already exists with this id overwriting the beacon information")
+            logging.warning(
+                "a beacon already exists with this id overwriting the beacon information")
         self._beacons[uid] = Beacon(uid, location)
 
     def add_measurement(self, beacon_id, position, reading):
@@ -72,39 +70,51 @@ class Map:
         return self._reference_points
 
 
-def binary_set(A: set):
+def binary_set(A):
+    '''
+    get the binary set of A
+
+            Parameters:
+                    A (list) list of tuples of beacon_id to rssi values
+            Returns:
+                the set of beacon tuples 
+    '''
     new_set = {}
     set_length = len(A)
     for i in range(set_length):
         for j in range(i + 1, set_length):
-            new_set.add((A[i], A[j]))
+            new_set.add((A[i][0], A[j][0]))
     return new_set
 
 
-def reversions(A: set, B: set):
+def reversions(A, B):
     '''
     get the number of discordant pairs
 
             Parameters:
-                    A (set) the set of avg_rssi values
-                    B (set) the set of avg_rssi values
+                    A (list) list of tuples of beacon_ids
+                    B (list) list of tuples of beacon_ids
     '''
     revs = 0
-    for avg_rssi in A:
-        # todo should use a sensor model to detect if rssi is the same
+
+    for _, avg_rssi in A:
         if not (avg_rssi in B):
             revs += 1
 
     return revs
 
 
-def tau(revs: int, A: set):
-    return 1 - 2 * revs / (len(A) * (len(A) - 1))
-
-
 def predict_position(taus, positions):
-    constants = taus / sum(taus)
-    average_position = sum([constant * position for constant, position in zip(constants, positions)])
+    '''
+    predicts position of a point using the weighted positions 
+
+            Parameters:
+                    taus (list) the weights for each positions
+                    positions (list) the cooridinate positions of the beacons
+    '''
+    tau_sum = sum(taus)
+    average_position = sum(
+        [constant * position/tau_sum for constant, position in zip(taus, positions)])
     return average_position
 
 
@@ -116,12 +126,18 @@ def calculate_tau(measured_rssi_readings, reference_point):
                     measured_rssi_readings (dict) the map of beacon ids to rssi values
                     reference_point (Point) the reference point we are comparing with
     '''
-    measured_rssi_readings = dict(sorted(measured_rssi_readings.items(), key=lambda item: item[1], reverse=True))
-    point_rssi_readings = dict(sorted(reference_point.rssi_map.items(), key=lambda item: item[1], reverse=True))
+    measured_point_readings = len(measured_rssi_readings.keys())
+    measured_rssi_readings = sorted(
+        measured_rssi_readings.items(), key=lambda item: item[1], reverse=True)
+    point_rssi_readings = sorted(
+        reference_point.rssi_map.items(), key=lambda item: item[1], reverse=True)
 
-    b_set = binary_set(measured_rssi_readings.values())
-    revs = reversions(measured_rssi_readings.values(), b_set)
-    tau = 1 - 2 * revs / (len(measured_rssi_readings) * (len(measured_rssi_readings) - 1))
+    measured_b_set = binary_set(measured_rssi_readings)
+    point_b_set = binary_set(point_rssi_readings)
+    revs = reversions(measured_b_set, point_b_set)
+
+    tau = 1 - 2 * revs / (len(measured_point_readings) *
+                          (len(measured_point_readings) - 1))
     return tau
 
 
@@ -139,10 +155,10 @@ def main():
     reference_points = area_map.reference_points
     tau_map = []
     for ref_point in reference_points:
-        tau_map.append((ref_point.location, calculate_tau(measured_rssi_readings, ref_point)))
+        tau_map.append((ref_point.location, calculate_tau(
+            measured_rssi_readings, ref_point)))
 
-
-    #larger tau suggests closer
+    # larger tau suggests closer
     sorted_taus = sorted(tau_map, key=lambda item: item[1], reverse=True)
 
     # find k nearest reference points
