88a9d4ffe5f9ffa6d8f43d7bba1a3c1bf99ae5f4
Thomas Barker <thomasjakebarker@outlook.com>
2021-12-28 16:27:41 +0000

    added bleutooth scanning functionality and advertising script

---
+++ advertising.bash
@@ -0,0 +1,11 @@
+#Run as root reference bluetooth core specification for more details vol 4 ,7.8.5
+#https://stackoverflow.com/questions/21124993/is-there-a-way-to-increase-ble-advertisement-frequency-in-bluez
+
+
+hciconfig hci0 up
+#advertising packet data
+hcitool -i hci0 cmd 0x08 0x0008 1E 02 01 06 1A FF 4C 00 02 15 C7 C1 A1 BF BB 00 4C AD 87 04 9F 2D 29 17 DE D2 00 00 00 00 C8 00
+# sets advertsing rate to 100ms 
+hcitool -i hci0 cmd 0x08 0x0006 A0 00 A0 00 03 00 00 00 00 00 00 00 00 07 00
+#LE Set Advertise Enable
+hcitool -i hci0 cmd 0x08 0x000a 01
--- src/BluetoothService.py
+++ src/BluetoothService.py
@@ -1,41 +1,83 @@
-import subprocess
-import parse
+import logging
+from bluepy.btle import Scanner, DefaultDelegate
+import numpy as np
+import scipy
 
 BEACON_PREFIX = "geolocation_beacon"
 
 
-class BluetoothService:
+class ScanDelegate(DefaultDelegate):
 
-    def __init__(self) -> None:
+    entries={}
+    def __init__(self):
+        DefaultDelegate.__init__(self)
 
-        process = subprocess.run(
-            ["btmgmt", "find"], capture_output=True, encoding="utf-8", text=True)
 
-        if process.returncode == 0:
-            device_strings = process.stdout.replace(
-                "\n", "").split("hci0 dev_found: ")[1:]
-            # print(process.stdout)
+    def handleDiscovery(self, dev, isNewDev, isNewData):
+        if isNewDev:
+            logging.debug(f"Discovered device address: {dev.addr} rssi: {dev.rssi}")
+        elif isNewData:
+            logging.debug(f"received new data device address: {dev.addr} rssi: {dev.rssi}")
 
-            c = parse.compile("\nhci0 dev_found: {address} type {type:S} rssi {rssi:d} flags {flags:x} \nname {name:S}")
-            for each in c.findall(process.stdout):
-                print(each)
-            devices = [self.parse_device_string(
-                device_string) for device_string in device_strings]
-            # 'print(devices)
+        devcice_entries = self.entries.get(dev.addr)
+        if devcice_entries is None:
+            self.entries[dev.addr] = [dev.rssi]
         else:
-            print(f"bluetooth was unable to scan for devices")
-
-    def parse_device_string(self, device_string):
-        device = {}
-        words = device_string.split()
-        # device["address"] = words[0]
-        device["address"] = ":".join(parse.search("{}:{}:{}:{}:{}:{} ", device_string).fixed)
-        device["type"] = parse.search("type {:S} ", device_string).fixed[0]
-        device["rssi"] = parse.search("rssi {:d} ", device_string).fixed[0]
-        name_parse = parse.search("name {:S}", device_string)
-        device["name"] = name_parse.fixed[0] if isinstance(name_parse, parse.Result) else None
-
-        return device
+            self.entries[dev.addr].append(dev.rssi)
 
 
-BluetoothService()
+def create_beacon_survey_maps(measurements: dict):
+
+    #p_distance = lambda X: np.product([scipy.stats.norm.pdf(abs(X[i+1]-X[i]), 0.1*1.5, 0.1*0.2) for i in range(len(X)-1)])
+    p_orientation = lambda X: np.product([])
+
+    for beacon,observations in measurements.items():
+        print(beacon)
+        print(observations)
+
+
+def calculate_cell_probabilities(measurements, beacon_survey_maps):
+    cell_probabilties = np.zeros((20,20))
+    standard_deviation = 1
+
+    for i in range(20):
+        for j in range(20):
+            position = (i,j)
+            distance = 0
+            n= len(measurements)
+            for beacon,measurement in measurements.items():
+                distance+= (measurement - beacon_survey_maps[beacon](position))**2
+            distance = distance/n
+
+            p = np.exp(-np.exp2(distance)/(2*np.exp2(standard_deviation)))
+            cell_probabilties[i,j] = p
+
+
+    return cell_probabilties
+
+
+
+
+def main():
+    logging.basicConfig(filename='bluetooth.log', level=logging.ERROR)
+
+    for i in range(1):
+        delegate= ScanDelegate()
+        scanner = Scanner().withDelegate(delegate)
+        
+        for i in range(10):
+            devices = scanner.scan(0.1)
+
+        print(delegate.entries)
+        """
+
+        # generate map with entries
+        survey_maps = create_beacon_survey_maps(delegate.entries)
+        average_measurements = {beacon:np.mean(measurements) for beacon,measurements in delegate.entries.items()}
+
+        cell_probabilties = np.zeros((20,20))
+        cell_probabilties = calculate_cell_probabilities(average_measurements,survey_maps)
+        most_likely_cell = np.argmax(cell_probabilties)"""
+        
+if __name__ == "__main__":
+    main()
