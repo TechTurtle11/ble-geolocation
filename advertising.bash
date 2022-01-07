50125885d9982f3abe2ba8208099c1424bcf50d4
Thomas Barker <thomasjakebarker@outlook.com>
2022-01-07 00:00:05 +0000

    script updates

--- advertising.bash
+++ advertising.bash
@@ -4,6 +4,12 @@
 #https://stackoverflow.com/questions/21124993/is-there-a-way-to-increase-ble-advertisement-frequency-in-bluez
 
 
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+
 hciconfig hci0 up
 #advertising packet data
 hcitool -i hci0 cmd 0x08 0x0008 1E 02 01 06 1A FF 4C 00 02 15 C7 C1 A1 BF BB 00 4C AD 87 04 9F 2D 29 17 DE D2 00 00 00 00 C8 00
--- install.bash
+++ install.bash
@@ -1,12 +1,9 @@
-
-
 #!/bin/bash
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 
    exit 1
 fi
 
-#!/bin/bash
 apt-get install python3 python3-dev bluez
 
 pip3 install -r requirements.txt
