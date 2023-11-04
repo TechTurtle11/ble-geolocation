92bac03fbc70ce4cfeae01d8ca47e58d58fa2756
Thomas Barker <thomasjakebarker@outlook.com>
2023-11-04 22:19:39 +0000

    add point stuct

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,11 @@ mod utils;
 fn main() {
     println!("Hello, world!");
 
-    println!("model is {}", utils::constants::Model::GAUSSIAN as u32)
+    let p = utils::Point {x: 5, y:8};
+
+
+
+    println!("hash is {}", p.hash())
 
 }
 
--- src/utils.rs
+++ src/utils.rs
@@ -1,1 +1,13 @@
 pub mod constants;
+
+pub struct Point {
+    pub x : i32,
+    pub y: i32
+}
+
+impl Point {
+
+    pub fn hash(self) -> f64 {
+        ((self.x.pow(2) + self.y.pow(2)) as f64).sqrt()
+    }
+}
